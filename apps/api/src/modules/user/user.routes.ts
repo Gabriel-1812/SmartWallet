3a5045a643ab92302eb60ce907950fe4128fcb12
Gabriel-1812 <gfredericoaraujo@gmail.com>
2026-02-12 13:20:52 -0300

    feat: add list users and get user by id endpoints

--- apps/api/src/modules/user/user.routes.ts
+++ apps/api/src/modules/user/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router, type Router as ExpressRouter } from 'express';
-import { createUserController } from './user.controller';
+import { createUserController, listUsersController, getUserByIdController } from './user.controller';
 
 const userRoutes: ExpressRouter = Router();
 
 userRoutes.post('/', createUserController)
+userRoutes.get('/', listUsersController)
+userRoutes.get('/:id', getUserByIdController)
 
 export { userRoutes };
--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -1,13 +1,15 @@
 import { userRepository } from "./user.repository";
 
-type CreateUserDTO = {
-    name: string;
-    email: string;
-    password: string
-}
-
 export const userService = {
-    createUser: async (data: CreateUserDTO) => {
+    createUser: async (data:{name:string; email: string; password:string})=> {
         return userRepository.create(data)
+    },
+
+    listUsers: async() => {
+        return userRepository.findAll()
+    },
+
+    getUserById: async(id:string) => {
+        return userRepository.findById(id)
     }
 }
